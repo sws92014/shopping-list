dcacc57c9df9719957c0b90f140c0cfd7b1dc3f4
Sarah Smith <sws92014@gmail.com>
2016-06-30 00:38:05 -0500

    adding js list function

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#shopping-form").on("submit", function(e){
+		e.preventDefault();
+		var itemToAdd = $(this).parent().children(".add-item__input").val();
+		if (itemToAdd != ""){
+			console.log(itemToAdd);
+			$(".need-list").prepend(
+				"<li class=\"sl-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
+				+itemToAdd+"</span><span class=\"side ex\">&#x2717;</span></li>");
+			$(this).parent().children(".add-item__input").val("");
+		}
+		else {
+			alert("Please type in the box to add items");
+		}
+	});
+		console.log("it works"); 
+
+	$(".need-list").on("click",".need-list-item > .check",function(){
+		purchasedItem = $(this).parent(".need-list-item");
+	});
+});
