5a0b8113978f86d047202f29b109cefc84a836b7
Sarah Smith <sws92014@gmail.com>
2016-07-07 00:33:18 -0500

    added a whole bunch

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	$("#shopping-form").on("submit", function(e){
 		e.preventDefault();
-		var itemToAdd = $(this).parent().children(".add-item__input").val();
+		var itemToAdd = $(".add-item__input").val();
 		if (itemToAdd != ""){
 			console.log(itemToAdd);
 			$(".need-list").prepend(
 				"<li class=\"need-list-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
 				+itemToAdd+"</span><span class=\"side ex\">&#x2717;</span></li>");
-			$(this).parent().children(".add-item__input").val("");
+			$(".add-item__input").val("");
 		}
 		else {
 			alert("Please type in the box to add items");
@@ -17,5 +17,36 @@ $(document).ready(function(){
 
 	$(".need-list").on("click",".need-list-item > .check",function(){
 		purchasedItem = $(this).parent(".need-list-item");
+		$(this).parent(".need-list-item").remove();
 	});
+
+	$(document).on("click", ".side.ex", function(){
+		$(this).parent(".need-list-item").remove();
+	})
+
+	$(document).on("click", ".side.check", function(){
+		var itemText = $(this).next().text();
+		if (itemText != ""){
+		$(".done-list").append(
+			"<li class=\"done-list-item\"><span class=\"side back-up\">&#x21ea;</span><span class=\"middle\">"
+				+itemText+"</span><span class=\"side ex\">&#x2717;</span></li>");
+			$(".add-item__input").val("");
+		}
+	})
+
+	$(document).on("click", ".side.ex", function(){
+		$(this).parent(".done-list-item").remove();
+	})
+
+	$(document).on("click", ".side.back-up", function(){
+		var itemToAdd = $(this).next().text();
+		if (itemToAdd != ""){
+		$(".need-list").append(
+			"<li class=\"need-list-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
+				+itemToAdd+"</span><span class=\"side ex\">&#x2717;</span></li>");
+			$(".add-item__input").val("");
+			$(this).parent(".done-list-item").remove();
+		}
+	})
+
 });
